0b5ad84a9fa4dd772b5dab965096064d203f9722
nbraunsc <nbraunsc@vt.edu>
2022-06-13 16:42:56 -0400

    added run files for arc

---
+++ he_clusters/08_05/run_tpsci.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=16
+#SBATCH -t 02:00:00
+#SBATCH -p normal_q
+#SBATCH -A nmayhall_group
+##SBATCH --exclusive
+#SBATCH --job-name 08_05
+FILE=tpsci
+
+# to see a list of available modules.
+module load Julia/1.5.1-linux-x86_64
+module load Anaconda3/2020.11
+source activate tpsci
+
+# Change to the directory from which the job was submitted
+cd $SLURM_SUBMIT_DIR
+
+date
+julia --project=../../../FermiCG/.  -t 16  $FILE.jl  > $FILE.out
+date
+
+exit;
