f9dfa949869ec1b2ff3bf393167ead393c276d7b
nbraunsc <nbraunsc@vt.edu>
2022-07-15 10:05:05 -0400

    adding integrals for N32 and N23 tetracence tetramere

---
+++ tetracene_tetramer/cluster_3_N32/run_tpsci.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+#SBATCH -p normal_q
+#SBATCH --cpus-per-task=16
+#SBATCH -t 10:00:00
+#SBATCH --account=nmayhall_group
+##SBATCH --account=nmayhall_group-paid
+#SBATCH --job-name N32_orbs
+#SBATCH --exclusive
+
+#export NTHREAD=16
+#export JULIAENV=/home/nbraunsc/FermiCG/
+
+
+# allow time to load modules, this takes time sometimes for some reason
+sleep 10
+
+hostname
+
+module reset
+module load site/tinkercliffs-rome/easybuild/setup  #only for infer
+module load site/tinkercliffs/easybuild/setup  #only for infer
+#module load Python/3.8.6-GCCcore-10.2.0
+#module load Julia/1.7.2-linux-x86_64
+module load Anaconda3/2020.07
+module load gcc/8.2.0
+
+source activate mim_env
+
+
+echo "Usage: sbatch submit.sh {input file} {data file} {data file}"
+
+# set these to 1 if you are using julia threads heavily to avoid oversubscription
+export MKL_NUM_THREADS=1
+export OMP_NUM_THREADS=1
+export OPENBLAS_NUM_THREADS=1
+
+export INFILE=$1
+export OUTFILE="${INFILE}.out"
+export WORKDIR=$(pwd)
+
+echo $INFILE
+echo $OUTFILE
+echo $WORKDIR
+echo $TMPDIR
+
+
+#julia --project=$JULIAENV -t $NTHREAD $INFILE >& $OUTFILE
+python nat_orb_active_space.py > nat_orb_active_space.out
+
+exit
+
