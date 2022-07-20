be44d4d0500b9586564425791bc4a67d379a4e95
Nicole Braunscheidel <nbraunsc@vt.edu>
2022-07-20 13:28:00 -0400

    updated cluster N24 notebook

--- tetracene_tetramer/cluster_3_N24/run_tpsci.sh
+++ tetracene_tetramer/cluster_3_N24/run_tpsci.sh
@@ -9,8 +9,8 @@
 #SBATCH --job-name N24
 #SBATCH --exclusive
 
-#export NTHREAD=16
-#export JULIAENV=/home/nbraunsc/FermiCG/
+export NTHREAD=16
+export JULIAENV=/home/nbraunsc/FermiCG/
 
 
 # allow time to load modules, this takes time sometimes for some reason
@@ -19,15 +19,8 @@ sleep 10
 hostname
 
 module reset
-module load site/tinkercliffs-rome/easybuild/setup  #only for infer
-module load site/tinkercliffs/easybuild/setup  #only for infer
-#module load Python/3.8.6-GCCcore-10.2.0
-#module load Julia/1.7.2-linux-x86_64
-module load Anaconda3/2020.07
-module load gcc/8.2.0
-
-source activate mim_env
-
+module load Python/3.8.6-GCCcore-10.2.0
+module load Julia/1.7.2-linux-x86_64
 
 echo "Usage: sbatch submit.sh {input file} {data file} {data file}"
 
@@ -45,9 +38,35 @@ echo $OUTFILE
 echo $WORKDIR
 echo $TMPDIR
 
+cp $INFILE $TMPDIR/
+if [ "$2" ]
+then
+        cp $2 $TMPDIR/
+fi
+if [ "$3" ]
+then
+        cp $3 $TMPDIR/
+fi
+cd $TMPDIR
 
-#julia --project=$JULIAENV -t $NTHREAD $INFILE >& $OUTFILE
-python nat_orb_active_space.py > nat_orb_active_space.out
+
+#Start an rsync command which runs in the background and keeps a local version of the output file up to date
+touch $OUTFILE
+while true; do rsync -av $OUTFILE $WORKDIR/"${INFILE}.${SLURM_JOB_ID}.out"; sleep 60; done &
+
+
+julia --project=$JULIAENV -t $NTHREAD $INFILE >& $OUTFILE
+
+cp $OUTFILE $WORKDIR/"${INFILE}.out"
+rm $WORKDIR/"${INFILE}.${SLURM_JOB_ID}.out"
+
+mkdir $WORKDIR/"${INFILE}.${SLURM_JOB_ID}.scr"
+
+cp -r * $WORKDIR/"${INFILE}.${SLURM_JOB_ID}.scr"
+rm -r *
+
+#moving standard output slurm file to specific job directory
+mv $WORKDIR/"slurm-${SLURM_JOB_ID}.out" $WORKDIR/"${INFILE}.${SLURM_JOB_ID}.scr"
 
 exit
 
