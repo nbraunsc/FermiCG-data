61ac50bf63dfb4600bcc4c10742f780081c390df
Nicole Braunscheidel <nbraunsc@vt.edu>
2023-09-15 13:44:48 -0400

    updated ras and fci tps scripts

--- he_clusters/he8_2cluster/tpsci_fci.jl
+++ he_clusters/he8_2cluster/tpsci_fci.jl
@@ -21,11 +21,11 @@ ansatze = [FCIAnsatz(3,3,3), FCIAnsatz(2,1,1), FCIAnsatz(3,0,0), FCIAnsatz(3,3,3
 # Build Cluster basis
 cluster_bases = FermiCG.compute_cluster_eigenbasis_spin(ints, clusters, d1, [8,8,8,8,8,8], ref_fock, ansatze, max_roots=M, verbose=0);
 
-nroots = 19
+nroots = 1
 ci_vector = FermiCG.TPSCIstate(clusters, FermiCG.FockConfig(init_fspace), R=nroots);
 
 # Add the lowest energy single exciton to basis
-ci_vector[FermiCG.FockConfig(init_fspace)][FermiCG.ClusterConfig([1,1,1,1,1,1])] = zeros(Float64,nroots)
+#ci_vector[FermiCG.FockConfig(init_fspace)][FermiCG.ClusterConfig([1,1,1,1,1,1])] = zeros(Float64,nroots)
 
 add_fock_configs_for_rasci(ref_fock, ci_vector)
 FermiCG.expand_each_fock_space!(ci_vector, cluster_bases)
@@ -42,40 +42,38 @@ cluster_ops = FermiCG.compute_cluster_ops(cluster_bases, ints);
 # Add cmf hamiltonians for doing MP-style PT2 
 FermiCG.add_cmf_operators!(cluster_ops, cluster_bases, ints, d1.a, d1.b, verbose=0);
 
-e0b, v0b = FermiCG.tpsci_ci(ci_vector, cluster_ops, clustered_ham,
-                            incremental  = true,
-                            thresh_cipsi = 0.0001,
-                            thresh_foi   = 1e-6,
-                            thresh_asci  = -1,
-                            #threaded = false,
-                            ci_max_iter = 200,
-                            max_mem_ci = 30.0);
+e0b, v0b = FermiCG.tps_ci_direct(ci_vector, cluster_ops, clustered_ham, conv_thresh=1e-8);
 
-#@time e2 = FermiCG.compute_pt2_energy(v0b, cluster_ops, clustered_ham, thresh_foi=1e-8)
-#@time e2 = FermiCG.compute_pt2_energy(v0b, cluster_ops, clustered_ham, thresh_foi=1e-8, verbose=0)
-e2 = 0
+#e0b, v0b = FermiCG.tpsci_ci(ci_vector, cluster_ops, clustered_ham,
+#                            incremental  = true,
+#                            thresh_cipsi = 0.0001,
+#                            thresh_foi   = 1e-5,
+#                            thresh_asci  = -1,
+#                            #threaded = false,
+#                            ci_max_iter = 200,
+#                            max_mem_ci = 50.0);
 
-println()
-println("   *======TPSCI results======*")
-@printf("TCI Thresh: %8.6f  Dim:%8d\n",0.0001,size(v0b)[1])
-println()
-@printf("TCI %5s %12s %12s\n", "Root", "E(0)", "E(2)")
-for r in 1:nroots
-    @printf("TCI %5s %12.8f %12.8f\n",r, e0b[r] + ecore, e0b[r] + ecore)
-    #@printf("TCI %5s %12.8f %12.8f\n",r, e0b[r] + ecore, e0b[r] + e2[r] + ecore)
-end
-
-clustered_S2 = FermiCG.extract_S2(ci_vector.clusters)
-
-println()
-println("   *======TPSCI S2 results======*")
-@printf(" %-50s", "Compute FINAL S2 expectation values: ")
-@time s2 = FermiCG.compute_expectation_value_parallel(v0b, cluster_ops, clustered_S2)
-
-@printf(" %5s %12s %12s\n", "Root", "Energy", "S2")
-for r in 1:nroots
-    @printf(" %5s %12.8f %12.8f\n",r, e0b[r]+ecore, abs(s2[r]))
-end
+#println()
+#println("   *======TPSCI results======*")
+#@printf("TCI Thresh: %8.6f  Dim:%8d\n",0.0001,size(v0b)[1])
+#println()
+#@printf("TCI %5s %12s %12s\n", "Root", "E(0)", "E(2)")
+#for r in 1:nroots
+#    @printf("TCI %5s %12.8f %12.8f\n",r, e0b[r] + ecore, e0b[r] + ecore)
+#end
+#
+#clustered_S2 = FermiCG.extract_S2(ci_vector.clusters)
+#
+#println()
+#println("   *======TPSCI S2 results======*")
+#@printf(" %-50s", "Compute FINAL S2 expectation values: ")
+#@time s2 = FermiCG.compute_expectation_value_parallel(v0b, cluster_ops, clustered_S2)
+#
+#@printf(" %5s %12s %12s\n", "Root", "Energy", "S2")
+#for r in 1:nroots
+#    @printf(" %5s %12.8f %12.8f\n",r, e0b[r]+ecore, abs(s2[r]))
+#end
 #@save "/Users/nicole/My Drive/code/FermiCG-data/he_clusters/he8_2cluster/fci_tpsci_notspincompleted_de4.jld2" cluster_bases e0b v0b s2 ecore
-@save "/Users/nicole/My Drive/code/FermiCG-data/he_clusters/he8_2cluster/fci_tpsci_19roots_3A.jld2" cluster_bases e0b v0b s2 e2 ecore
+#@save "/Users/nicole/My Drive/code/FermiCG-data/he_clusters/he8_2cluster/fci_tpsci_3A_1root.jld2" cluster_bases e0b v0b s2 ecore
+@save "/Users/nicole/My Drive/code/FermiCG-data/he_clusters/he8_2cluster/fci_tpsci_3A_1body.jld2" e0b v0b 
 #@save "/Users/nicole/My Drive/code/FermiCG-data/he_clusters/he8_2cluster/fci_tpsci.jld2" cluster_bases e0b v0b s2 ecore
--- he_clusters/he8_2cluster/tpsci_ras.jl
+++ he_clusters/he8_2cluster/tpsci_ras.jl
@@ -28,9 +28,9 @@ ansatze = [RASCIAnsatz(8, 4, 4, (3,2,3), max_h=1, max_p=1), RASCIAnsatz(8,4,4,(3
 # Build Cluster basis
 cluster_bases = FermiCG.compute_cluster_eigenbasis_spin(ints, clusters, d1, [0,0], ref_fock, ansatze, max_roots=M, verbose=1);
 #
-nroots = 19
+nroots = 1
 ci_vector = FermiCG.TPSCIstate(clusters, FermiCG.FockConfig(init_fspace), R=nroots);
-ci_vector[FermiCG.FockConfig(init_fspace)][FermiCG.ClusterConfig([1,1])] = zeros(Float64,nroots)
+#ci_vector[FermiCG.FockConfig(init_fspace)][FermiCG.ClusterConfig([1,1])] = zeros(Float64,nroots)
 FermiCG.expand_each_fock_space!(ci_vector, cluster_bases)
 FermiCG.eye!(ci_vector)
 
@@ -45,35 +45,37 @@ cluster_ops = FermiCG.compute_cluster_ops(cluster_bases, ints);
 # Add cmf hamiltonians for doing MP-style PT2 
 FermiCG.add_cmf_operators!(cluster_ops, cluster_bases, ints, d1.a, d1.b, verbose=0);
 
-e0b, v0b = FermiCG.tpsci_ci(ci_vector, cluster_ops, clustered_ham,
-                            incremental  = true,
-                            thresh_cipsi = 0.0001,
-                            thresh_foi   = 1e-6,
-                            thresh_asci  = -1,
-                            ci_max_iter = 200,
-                            max_mem_ci = 30.0);
+e0b, v0b = FermiCG.tps_ci_direct(ci_vector, cluster_ops, clustered_ham, conv_thresh=1e-8);
 
+#e0b, v0b = FermiCG.tpsci_ci(ci_vector, cluster_ops, clustered_ham,
+#                            incremental  = true,
+#                            thresh_cipsi = 0.0001,
+#                            thresh_foi   = 1e-5,
+#                            thresh_asci  = -1,
+#                            ci_max_iter = 200,
+#                            max_mem_ci = 50.0);
+#
 #@time e2 = FermiCG.compute_pt2_energy(v0b, cluster_ops, clustered_ham, thresh_foi=1e-8, verbose=0)
-e2=0
-println()
-println("   *======TPSCI results======*")
-@printf("TCI Thresh: %8.6f  Dim:%8d\n",0.0001,size(v0b)[1])
-println()
-@printf("TCI %5s %12s %12s\n", "Root", "E(0)", "E(2)")
-for r in 1:nroots
-    @printf("TCI %5s %12.8f %12.8f\n",r, e0b[r] + ecore, e0b[r] + ecore)
-    #@printf("TCI %5s %12.8f %12.8f\n",r, e0b[r] + ecore, e0b[r] + e2[r] + ecore)
-end
 
-clustered_S2 = FermiCG.extract_S2(ci_vector.clusters)
-
-println()
-println("   *======TPSCI S2 results======*")
-@printf(" %-50s", "Compute FINAL S2 expectation values: ")
-@time s2 = FermiCG.compute_expectation_value_parallel(v0b, cluster_ops, clustered_S2)
-
-@printf(" %5s %12s %12s\n", "Root", "Energy", "S2")
-for r in 1:nroots
-    @printf(" %5s %12.8f %12.8f\n",r, e0b[r]+ecore, abs(s2[r]))
-end
-@save "/Users/nicole/My Drive/code/FermiCG-data/he_clusters/he8_2cluster/ras_tpsci_singles_19roots_3A.jld2" cluster_bases e0b v0b s2 e2 ecore
+#println()
+#println("   *======TPSCI results======*")
+#@printf("TCI Thresh: %8.6f  Dim:%8d\n",0.0001,size(v0b)[1])
+#println()
+#@printf("TCI %5s %12s %12s\n", "Root", "E(0)", "E(2)")
+#for r in 1:nroots
+#    @printf("TCI %5s %12.8f %12.8f\n",r, e0b[r] + ecore, e0b[r] + ecore)
+#end
+#
+#clustered_S2 = FermiCG.extract_S2(ci_vector.clusters)
+#
+#println()
+#println("   *======TPSCI S2 results======*")
+#@printf(" %-50s", "Compute FINAL S2 expectation values: ")
+#@time s2 = FermiCG.compute_expectation_value_parallel(v0b, cluster_ops, clustered_S2)
+#
+#@printf(" %5s %12s %12s\n", "Root", "Energy", "S2")
+#for r in 1:nroots
+#    @printf(" %5s %12.8f %12.8f\n",r, e0b[r]+ecore, abs(s2[r]))
+#end
+#@save "/Users/nicole/My Drive/code/FermiCG-data/he_clusters/he8_2cluster/ras_tpsci_3A_1root.jld2" cluster_bases e0b v0b s2 ecore
+@save "/Users/nicole/My Drive/code/FermiCG-data/he_clusters/he8_2cluster/ras_tpsci_3A_1body.jld2" e0b v0b
